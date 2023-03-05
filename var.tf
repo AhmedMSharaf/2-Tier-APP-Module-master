73760951e6fed1892b5eca8fd863c03b9f1adfd0
AhmedMSharaf <55796502+AhmedMSharaf@users.noreply.github.com>
2023-03-05 20:48:10 +0200

    Update var.tf

--- var.tf
+++ var.tf
@@ -10,17 +10,17 @@ variable "subnets" {
     public-subnet = {
         "name" = "public-subnet"
         "availability_zone" = "eu-central-1a"
-        "cidr_block" = "10.0.4.0/24"
+        "cidr_block" = "10.10.4.0/24"
     }
     private-subnet-1 = {
         "name" = "private-subnet-1"
         "availability_zone" = "eu-central-1b"
-        "cidr_block" = "10.0.5.0/24"
+        "cidr_block" = "10.10.5.0/24"
     }
     private-subnet-2 = {
         "name" = "private-subnet-2"
         "availability_zone" = "eu-central-1ac"
-        "cidr_block" = "10.0.6.0/24"
+        "cidr_block" = "10.10.6.0/24"
     }
   }
 }
